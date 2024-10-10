0701f3fcb35c1be6a29cb3aea12dfcf28c285a1c
Danilo oliveira <minhavez52@gmail.com>
2024-10-10 19:16:37 -0300

    exemplo de outras maneiras de chamar o js

---
+++ Arquivos/js/dandanda.js
@@ -0,0 +1,25 @@
+calcularMedia = (valor1, valor2) => (valor1+valor2)/2 ;
+
+let nome = prompt("Informe o nome do aluno:");
+let nota1 = prompt("Informe a primeira nota do aluno:");
+let nota2 = prompt("Informe a segunda nota do aluno:");
+
+media = calcularMedia(Number(nota1), Number(nota2));
+
+alert(`O aluno ${nome} teve média de ${media}`);
+if (media>=7) {
+    alert("Aprovado!");
+} else if (media>=5) {
+    alert("Recuperação");
+} else {
+    alert("Reprovado")
+    ;
+}
+
+
+
+
+// function calcularMedia (valor1, valor2) {
+//     let resultado = (valor1+valor2)/2;
+//     return resultado;
+// }
